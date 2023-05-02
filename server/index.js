456f7d30b75f873c3f5b1f26e1212ff044ab7142
Swathishree021 <swathigapp@gmail.com>
2023-05-02 15:48:44 +0530

    connected express server to mysql

--- server/index.js
+++ server/index.js
@@ -1,6 +1,16 @@
-const express = require('express')
-const app = express()
-const PORT = 3001
+const express = require('express');
+const app = express();
+//connecting express server to mysql db by calling a variable called mysql
+const mysql = require('mysql');
+const PORT = 3001;
+
+//create a variable called db
+const db = mysql.createConnection({
+    user: 'root',
+    host: 'localhost',
+    password: 'password',
+    datbase: 'passwordmanager' ,
+});
 
 
 app.get('/',(req,res) => {
