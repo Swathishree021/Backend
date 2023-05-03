4b532261aaab0bab0a6be2d2fb136657f95cc0b7
Swathishree021 <swathigapp@gmail.com>
2023-05-03 10:51:03 +0530

    added hooks & styling for forntend

--- client/src/App.js
+++ client/src/App.js
@@ -11,8 +11,17 @@ const [title, setTitle] = useState('')
     <div className="App">
    {/* Adding form for new passwords */}
    <div className="AddingPassword">
-    <input type="text" placeholder='Ex.password123'/>
-    <input type="text" placeholder='Ex.vapt'/>
+    <input type="text"
+     placeholder="Ex.password123" 
+     onChange={(event)=>{setPassword(event.target.value);
+    }}
+    />
+    <input 
+    type="text" 
+    placeholder="Ex.vapt"
+    onChange={(event)=>{setTitle(event.target.value);
+    }}
+    />
     <button> Add password</button>
    </div>
     </div>
